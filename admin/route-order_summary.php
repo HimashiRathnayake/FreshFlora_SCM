550c82f5adb552cf91149d7bb3313a8604d56ad4
JananiSumanapala <jananisudeeptha@gmail.com>
2020-01-01 16:04:39 +0530

    some changes in files

--- admin/route-order_summary.php
+++ admin/route-order_summary.php
@@ -1,6 +1,6 @@
 <?php  
  $connect = mysqli_connect("localhost", "root", "", "scm_db");  
- $query = "SELECT area, count(distinct order_ID) as number FROM customer natural join orders natural join order_details natural join store_route on customer.route_ID = store_route.route_ID natural join route_area group by route_ID order by number DESC";  
+ $query = "SELECT destination, city, count(distinct order_ID) as number FROM customer natural join orders natural join order_details natural join store_route on customer.route_ID = store_route.route_ID natural join route_area natural join store group by route_ID order by number DESC";  
  $result = mysqli_query($connect, $query); 
  ?>
  <head>
@@ -8,14 +8,14 @@
 	<link href='https://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
  <table cellspacing="20">
  <tr>
-	<th>route id</th>
+	<th>Route</th>
 	<th>Number of orders</th>
 	</tr>
  <?php foreach ($result as $value){
 	?>
 	
 	<tr>
-	<td><?php echo $value["area"]; ?></td>
+	<td><?php echo $value["city"]+"-"+$value["destination"]; ?></td>
 	<td><?php echo $value["number"]; ?></td>
 	</tr>
 	<?php 
