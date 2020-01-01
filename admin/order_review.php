65859a7ed9a4bae8a7a9e4e41730510fcca917e4
JananiSumanapala <jananisudeeptha@gmail.com>
2020-01-01 17:55:23 +0530

    some changes

--- admin/order_review.php
+++ admin/order_review.php
@@ -46,7 +46,7 @@
            <br /><br />  
            <div style="width:1000px;">  
                 <h1 style = color :"pink; align="center" >Summary of orders by products</h1>
-                <h4> Most ordered product : <?php echo $row;?></h5>   
+                <h4> Most ordered product : <?php echo $row;?></h4>   
                 <br />  
                 <div id="piechart" style="width: 800px; height: 750px;"></div>  
            </div> 
