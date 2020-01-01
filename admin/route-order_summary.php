8fc62b03e20e2f9ac908eb0cd35b36fca1100d15
JananiSumanapala <jananisudeeptha@gmail.com>
2020-01-01 16:07:16 +0530

    some changes

--- admin/route-order_summary.php
+++ admin/route-order_summary.php
@@ -1,6 +1,6 @@
 <?php  
  $connect = mysqli_connect("localhost", "root", "", "scm_db");  
- $query = "SELECT destination, city, count(distinct order_ID) as number FROM customer natural join orders natural join order_details natural join store_route on customer.route_ID = store_route.route_ID natural join route_area natural join store group by route_ID order by number DESC";  
+ $query = "SELECT destination, city, count(distinct order_ID) as number FROM customer natural join orders natural join order_details natural join store_route natural join route_area natural join store group by route_ID order by number DESC";  
  $result = mysqli_query($connect, $query); 
  ?>
  <head>
